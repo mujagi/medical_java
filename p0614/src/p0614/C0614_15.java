c92758a9e06ecfaff53a1daccd9225c39e74eadf
mujagi <162073462+mujagi@users.noreply.github.com>
2024-06-14 18:15:11 +0900

    java - 변수, 연산자

---
+++ p0614/src/p0614/C0614_15.java
@@ -0,0 +1,15 @@
+package p0614;
+
+public class C0614_15 {
+
+	public static void main(String[] args) {
+		System.out.println((float)0.1d == 0.1f); 
+		double num = 0.1d;
+		System.out.println("double 타입 :"+num);
+		
+		double d = (double)0.1f;
+		System.out.println("float 타입변경 :"+d);
+		
+	}// main
+
+}// class
